075a3ba1a1205e5951feb3071dc5ee8b3d6d8851
Vitalii Prezent <79227315+Justfromhere@users.noreply.github.com>
2024-12-10 18:09:09 +0200

    #2

--- lab-09/lab-09.py
+++ lab-09/lab-09.py
@@ -1,18 +1,51 @@
 # Завдання 1
-dictionary = {}
-with open('dict.txt', 'r', encoding='utf-8') as dict_file:
-    for line in dict_file:
-        english_word, ukrainian_word = line.strip().split('\t-\t')
-        dictionary[english_word] = ukrainian_word
+# dictionary = {}
+# dict_file = open('dict.txt', 'r')
+# for line in dict_file:
+#     parts = line.split('\t-\t')  
+#     english_word = parts[0]  
+#     ukrainian_word = parts[1].strip()  
+#     dictionary[english_word] = ukrainian_word 
+# dict_file.close() 
 
-with open('translate.txt', 'r', encoding='utf-8') as translate_file:
-    text_to_translate = translate_file.read()
 
-translated_text = []
-for word in text_to_translate.split():
+# translate_file = open('translate.txt', 'r')
+# text_to_translate = translate_file.read() 
+# translate_file.close() 
 
-    translated_word = dictionary.get(word, word) 
-    translated_text.append(translated_word)
 
-with open('output.txt', 'w', encoding='utf-8') as output_file:
-    output_file.write(' '.join(translated_text))
+# translated_text = []  
+# words = text_to_translate.split()  
+# for word in words:
+#     if word in dictionary: 
+#         translated_text.append(dictionary[word])  
+#     else:
+#         translated_text.append(word)  
+
+
+# output_file = open('output.txt', 'w')
+# output_file.write(' '.join(translated_text))  
+# output_file.close()  
+
+# Завдання 2
+countries = {}
+
+with open('input_countries.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        sep = line.split(' : ')
+        country = sep[0]
+        lengs = sep[1]
+        countries[country] = lengs
+
+inputLeng = input('Введіть мову: ')
+
+result = []
+
+for country, lengs in countries.items():
+    if inputLeng in lengs:
+        result.append(country)
+
+if result:
+    print("Країни, де розмовляють цією мовою:", result)
+else:
+    print("Такої мови немає в списку.")
